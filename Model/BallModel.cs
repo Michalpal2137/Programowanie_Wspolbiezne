14cd35d09d1c87a9ac0c6196addf2e5abdb67a24
Amelia Pietrzak <251203@edu.p.lodz.pl>
2026-06-01 20:31:52 +0200

    etap1-fix

---
+++ Model/BallModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+
+namespace Model
+{
+    public class BallModel : INotifyPropertyChanged
+    {
+        private double _x;
+        private double _y;
+        private double _radius;
+
+        public double X
+        {
+            get => _x;
+            set
+            {
+                if (_x != value)
+                {
+                    _x = value;
+                    OnPropertyChanged(nameof(X));
+                }
+            }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                if (_y != value)
+                {
+                    _y = value;
+                    OnPropertyChanged(nameof(Y));
+                }
+            }
+        }
+
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (_radius != value)
+                {
+                    _radius = value;
+                    OnPropertyChanged(nameof(Radius));
+                    OnPropertyChanged(nameof(Diameter));
+                }
+            }
+        }
+
+        // Dodaj właściwość dla średnicy (2 × promień)
+        public double Diameter => Radius * 2;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
